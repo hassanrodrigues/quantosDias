18f063a5e63c8f772008a2e7dd9ec9dd859b12ee
hassanrodrigues <hassanrodrigues14@gmail.com>
2022-11-21 09:02:52 -0400

    ajusting code seconds

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 console.log("oiiiaa")
 let date1 = new Date("04/12/2020")
 let date2 = new Date(Date.now())
-console.log(date1,date2)
+console.log(date1, date2)
 let timeDiff = Math.abs(date2 - date1);
-let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 console.log(diffDays)
 
 let body = document.querySelector('body')
 let valor = document.querySelector('#dias')
 
-valor.innerText =  `${diffDays} dias ${date2.getHours()}h:${date2.getMinutes()}m:${date2.getSeconds()}s:${date2.getMilliseconds()}ms `
+valor.innerText = `${diffDays} dias ${date2.getHours()}h:${date2.getMinutes()}m:${date2.getSeconds()}s `
 //time real time
 setInterval(() => {
-      let date2 = new Date(Date.now())
-      valor.innerText =  `${diffDays} dias ${date2.getHours()}h:${date2.getMinutes()}m:${date2.getSeconds()}s `
+   let date2 = new Date(Date.now())
+   valor.innerText = `${diffDays} dias ${date2.getHours()}h:${date2.getMinutes()}m:${date2.getSeconds()}s `
 }, 1000);
 
 
-addEventListener('click',function(ev){
-   let img =ev.target.tagName
+addEventListener('click', function (ev) {
+   let img = ev.target.tagName
 
-   if(img=='IMG'){
-    console.log('oiiiii')
+   if (img == 'IMG') {
+      console.log('oiiiii')
    }
-
 })
